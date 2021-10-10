43331caa33bae2f61fc8597fcf800e5a9c2264eb
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-10-10 11:44:01 +0200

    feat: added eslint & prettier configurations

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-import * as dotenv from "dotenv";
-import { resolve } from "path";
+/* eslint-disable import/first */
 
-dotenv.config({ path: resolve(__dirname, "../.env") });
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
 
-import { Client, Intents } from "discord.js";
-import { _assert } from "./utils/_assert";
+dotenv.config({ path: resolve(__dirname, '../.env') });
+
+import { Client, Intents } from 'discord.js';
+import { _assert } from './utils/_assert';
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on("ready", () => {
-    _assert(client.user);
-    console.log(`Logged in as ${client.user.tag}!`);
+client.on('ready', () => {
+   _assert(client.user);
+   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
+client.on('interactionCreate', async (interaction) => {
+   if (!interaction.isCommand()) return;
 
-    if (interaction.commandName === "ping") {
-        await interaction.reply("Pong!");
-    }
+   if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+   }
 });
 
 client.login(process.env.DISCORD_TOKEN_PRODUCTION);
--- src/utils/_assert.ts
+++ src/utils/_assert.ts
@@ -1,22 +1,19 @@
-import assert from "assert";
+import assert from 'assert';
 
 /**
  * Checks that value is not null nor undefined.
  * @param value
  * @param message
  */
-export function _assert<T>(
-    value: T,
-    message?: string | Error
-): asserts value is NonNullable<T> {
-    if (typeof value === "boolean") {
-        throw new Error("Use _assertTrue for boolean instead.");
-    }
+export function _assert<T>(value: T, message?: string | Error): asserts value is NonNullable<T> {
+   if (typeof value === 'boolean') {
+      throw new Error('Use _assertTrue for boolean instead.');
+   }
 
-    if (typeof value === "boolean" || value === undefined || value === null) {
-        const err = typeof message === "object" ? message : new Error(message);
-        throw err;
-    }
+   if (typeof value === 'boolean' || value === undefined || value === null) {
+      const err = typeof message === 'object' ? message : new Error(message);
+      throw err;
+   }
 }
 
 /**
@@ -24,14 +21,11 @@ export function _assert<T>(
  * @param value
  * @param message
  */
-export function _assertTrue(
-    value: boolean,
-    message?: string | Error
-): asserts value is true {
-    if (value !== true) {
-        const err = typeof message === "object" ? message : new Error(message);
-        throw err;
-    }
+export function _assertTrue(value: boolean, message?: string | Error): asserts value is true {
+   if (value !== true) {
+      const err = typeof message === 'object' ? message : new Error(message);
+      throw err;
+   }
 }
 
 /**
@@ -41,9 +35,9 @@ export function _assertTrue(
  * @param message
  */
 export function _assertStrictEqual<T>(
-    actual: unknown,
-    expected: T,
-    message?: string | Error | undefined
+   actual: unknown,
+   expected: T,
+   message?: string | Error | undefined,
 ): asserts actual is T {
-    return assert.strictEqual(actual, expected, message);
+   return assert.strictEqual(actual, expected, message);
 }
