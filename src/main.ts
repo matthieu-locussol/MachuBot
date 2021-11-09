8be66fa24338e4d298903da5e37972d999903451
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-11-09 22:16:31 +0100

    fix: updated github workflow & reordered imports in main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+/* eslint-disable import/first */
+/* eslint-disable import/order */
+
 import * as dotenv from 'dotenv';
 import { resolve } from 'path';
-import { Bot } from './bot';
-import { pingModule } from './modules/ping';
 
 dotenv.config({ path: resolve(__dirname, '../.env') });
+
+import { Bot } from './bot';
+import { pingModule } from './modules/ping';
 
 const MachuBot = new Bot([pingModule]);
 MachuBot.start();
