fd97e494f9c683e64c86bb27c85fd45e8d77dd84
Matthieu Locussol <dev.matthieu.locussol@gmail.com>
2021-12-22 22:04:02 +0100

    feat: added osu & osu-tools-api status to the status command

--- src/modules/status/commands/status.ts
+++ src/modules/status/commands/status.ts
@@ -1,11 +1,24 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { ChatInputCommand } from '../../../types/commands';
 import { getEmoji } from '../../../utils/emoji';
 import { makeStatusEmbed } from '../utils/embeds/StatusEmbed';
 
 interface CoinGeckoStatusResponse {
    gecko_says: string;
+}
+
+interface OsuStatusResponse {
+   streams: {
+      display_name: string;
+      latest_build: {
+         display_version: string;
+      };
+   }[];
+}
+
+interface OsuToolsApiStatusResponse {
+   version: string;
 }
 
 export const statusCommand: ChatInputCommand = {
@@ -18,17 +31,45 @@ export const statusCommand: ChatInputCommand = {
 
       const guildManager = interaction.client.guilds;
 
-      const coinGeckoStatus = await axios.get<CoinGeckoStatusResponse>(
-         'https://api.coingecko.com/api/v3/global',
-      );
+      const [coinGeckoStatus, osuStatus, osuToolsApiStatus] = await Promise.all([
+         axios.get<CoinGeckoStatusResponse>('https://api.coingecko.com/api/v3/global'),
+         axios.get<OsuStatusResponse>('https://osu.ppy.sh/api/v2/changelog'),
+         axios.get<OsuToolsApiStatusResponse>(String(process.env.OSU_PP_ENDPOINT)),
+      ]);
+
+      const getStatusEmoji = (response: AxiosResponse) => {
+         if (response.status === 200) {
+            return getEmoji(guildManager, 'Correct');
+         }
+
+         return getEmoji(guildManager, 'Incorrect');
+      };
+
+      const getOsuVersion = (response: AxiosResponse<OsuStatusResponse>) => {
+         if (response.status === 200) {
+            const stream = osuStatus.data.streams[0];
+            return `${stream.display_name}(v${stream.latest_build.display_version})`;
+         }
+
+         return '';
+      };
+
+      const getOsuToolsApiVersion = (response: AxiosResponse<OsuToolsApiStatusResponse>) => {
+         if (response.status === 200) {
+            return `(v${response.data.version})`;
+         }
+
+         return '';
+      };
 
       await interaction.editReply({
          embeds: [
             makeStatusEmbed({
-               coinGeckoStatus: `${getEmoji(
-                  guildManager,
-                  coinGeckoStatus.status === 200 ? 'Correct' : 'Incorrect',
-               )}\tCoinGecko status`,
+               coinGeckoStatus: `${getStatusEmoji(coinGeckoStatus)}\tCoinGecko status`,
+               osuStatus: `${getStatusEmoji(osuStatus)}\tOsu status ${getOsuVersion(osuStatus)}`,
+               osuToolsApiStatus: `${getStatusEmoji(
+                  osuToolsApiStatus,
+               )}\tOsu-tools-api status ${getOsuToolsApiVersion(osuToolsApiStatus)}`,
             }),
          ],
       });
