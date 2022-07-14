cec8d1231882d0fc62fe245aff4b236504352834
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2022-07-14 22:31:55 +0200

    feat: improved modalHandler to display inputed values

--- src/modules/sample/components/handlers/modalHandler.ts
+++ src/modules/sample/components/handlers/modalHandler.ts
@@ -1,5 +1,14 @@
 import { ModalComponentHandler } from '../../../../types/modals';
 
 export const modalHandler: ModalComponentHandler = async (interaction) => {
-   interaction.reply('Modal submitted!');
+   const shortInputContent = interaction.fields.getTextInputValue('shortInput');
+   const paragraphInputContent = interaction.fields.getTextInputValue('paragraphInput');
+
+   interaction.reply({
+      content: [
+         'Modal submitted!',
+         `Short input content: \`${shortInputContent}\``,
+         `Paragraph input content: \`${paragraphInputContent}\``,
+      ].join('\n'),
+   });
 };
